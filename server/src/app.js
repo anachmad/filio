b9de150785c0ac9f26f8ebe382cc34d6130669db
Andik Nur Achmad <andik@192.168.0.5>
2025-10-06 06:38:45 +0700

    Menambahkan screen login, register, dan dashboard

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-// const userRoutes = require('./routes/user.routes');
 const authRoutes = require('./features/auth/auth.routes')
 
 const app = express();
@@ -7,7 +6,6 @@ const app = express();
 // middleware untuk baca json dari body request
 app.use(express.json());
 
-// app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
 module.exports = app;
--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,7 +1,5 @@
 const {PrismaClient} = require('@prisma/client');
 
-// import {PrismaClient} from '.src/generated/prisma'
-
 const prisma = new PrismaClient();
 
 module.exports = prisma;
