98f1e417c8677d4b9adb5adb87c377506233b036
Andik Nur Achmad <andik.achmad@gmail.com>
2025-10-08 06:59:30 +0700

    Feat : Determine which screen to show to user based on the saved token on the asyncstorage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import DashboardScreen from './src/screens/DashboardScreen';
+import AuthLoadingScreen from './src/screens/AuthLoadingScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  // Melacak status loading awal dan auth token
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState<string | null>(null);
+
+  // Memeriksa auth token di async storage dan memperbarui state
+  useEffect(() => {
+      const bootstrapAsync = async () => {
+        let token = null;
+
+        try {
+          token = await AsyncStorage.getItem('authToken');
+        
+        } catch (error) {
+          console.error('Gagal mengambil token', error);
+        }
+
+        setUserToken(token);
+        setIsLoading(false);
+      };
+
+      bootstrapAsync();
+
+  }, []);
+
+  // Menampilkan screen Loading jika masih dalam proses loading
+  if (isLoading) {
+    return <AuthLoadingScreen />
+  }
+
   return (
       <NavigationContainer>
-          <Stack.Navigator initialRouteName='Login'>
-            <Stack.Screen name = 'Login' component={LoginScreen} />
-            <Stack.Screen name = 'Register' component={RegisterScreen} />
-            <Stack.Screen name = 'Dashboard' component={DashboardScreen} />
+          <Stack.Navigator>
+            { userToken == null ? (
+              <>
+                <Stack.Screen
+                  name = 'Login'
+                  component = { LoginScreen }
+                  options = {{ headerShown: false }}
+                  />
+
+                <Stack.Screen
+                  name = 'Register'
+                  component = { RegisterScreen }
+                  options = {{ title: 'Daftar akun baru' }}
+                  />
+              </>
+            ) : (
+              // Menampilkan screen Dashboard jika token ada
+              <>
+                <Stack.Screen name = "Dashboard"
+                  component = { DashboardScreen }
+                />
+              </>
+            )}
+
           </Stack.Navigator>
       </NavigationContainer>
   );
