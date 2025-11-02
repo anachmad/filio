dc85291b3017ce44f58fb6b6e0239e07e5e4efdb
Andik Nur Achmad <andik.achmad@gmail.com>
2025-11-02 17:00:39 +0700

    Feat : add icon images, and add interceptor

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -1,7 +1,25 @@
 import axios from 'axios';
+import { Alert } from 'react-native';
 
 const apiClient = axios.create({
     baseURL: 'http://192.168.0.3:3000/api',
 });
+
+// Interceptor to handle responses
+apiClient.interceptors.request.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (!error.response) {
+            Alert.alert('Koneksi Gagal', 'Tidak dapat terhubung ke server. Pastikan Anda terhubung ke internet.');
+    } else {
+        if (error.response.status >= 500) {
+            Alert.alert('Kesalahan Server', 'Terjadi kesalahan pada server. Silakan coba lagi nanti.');
+        }
+    }
+        return Promise.reject(error);
+    }
+);
 
 export default apiClient;
